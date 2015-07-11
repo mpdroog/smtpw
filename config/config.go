f0f6a1c71fd1a1cd870b81aaa342476574c68d75
mark <mpdroog@rootdev.nl>
2015-07-11 11:58:30 +0200

    Feature. Initial commit

---
+++ config/config.go
@@ -0,0 +1,45 @@
+package config
+
+// Read config.json
+import (
+	"encoding/json"
+	"os"
+)
+
+type ConfigFrom struct {
+	User string
+	Pass string
+	Host string
+	Port int
+	From string
+	Display string
+	Bcc []string
+}
+type Config struct {
+	Beanstalk string
+	From map[string]ConfigFrom
+}
+
+type Email struct {
+	From string
+	To []string
+	Subject string
+	Html string
+	Text string
+	HtmlEmbed map[string]string // file.png => base64(bytes)
+}
+
+var (
+	C           Config
+)
+
+func Init(f string) error {
+	r, e := os.Open(f)
+	if e != nil {
+		return e
+	}
+	if e := json.NewDecoder(r).Decode(&C); e != nil {
+		return e
+	}
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"errors"
+	"encoding/json"
+	"encoding/base64"
+	"fmt"
+	"flag"
+	"gopkg.in/gomail.v1"
+	"github.com/maxid/beanstalkd"
+	"smtpw/config"
+	"time"
+	"strings"
+)
+
+const RESERVE_SEC = 20
+const ERR_WAIT_SEC = 5
+var verbose bool
+
+func proc(m config.Email) error {
+	conf, ok := config.C.From[m.From]
+	if !ok {
+		return errors.New("From does not exist: " + m.From)
+	}
+
+	msg := gomail.NewMessage()
+	msg.SetHeader("From", conf.Display)
+	msg.SetHeader("To", m.To...)
+	msg.SetHeader("Bcc", conf.Bcc...)
+	msg.SetHeader("Subject", m.Subject)
+	msg.SetBody("text/plain", m.Text)
+	msg.SetBody("text/html", m.Html)
+
+	for name, embed := range m.HtmlEmbed {
+		raw, e := base64.StdEncoding.DecodeString(embed)
+		if e != nil {
+			return errors.New("HtmlEmbed: " + name + " is not base64!")
+		}
+		if !strings.Contains(m.Html, fmt.Sprintf("cid:" + name)) {
+			return errors.New("HtmlEmbed: " + name + " is not used in the HTML!")
+		}
+		msg.Embed(gomail.CreateFile(name, raw))
+	}
+
+	mailer := gomail.NewMailer(conf.Host, conf.User, conf.Pass, conf.Port)
+	return mailer.Send(msg)
+}
+
+func main() {
+	var (
+		configPath string
+		skipOne bool
+	)
+	flag.BoolVar(&verbose, "v", false, "Verbose-mode")
+	flag.BoolVar(&skipOne, "s", false, "Delete e-mail on deverr")
+	flag.StringVar(&configPath, "c", "./config.json", "Path to config.json")
+	flag.Parse()
+
+	if e := config.Init(configPath); e != nil {
+		panic(e)
+	}
+	// TODO: Test config before starting?
+
+	queue, e := beanstalkd.Dial(config.C.Beanstalk)
+	if e != nil {
+		panic(e)
+	}
+
+	for {
+		job, e := queue.Reserve(RESERVE_SEC)
+		if e != nil {
+			fmt.Println("Beanstalkd err:" + e.Error())
+			time.Sleep(time.Second * ERR_WAIT_SEC)
+		}
+		if verbose {
+			fmt.Println(fmt.Sprintf("Parse job %d", job.Id))
+		}
+		// Parse
+		var m config.Email
+		if e := json.Unmarshal(job.Data, &m); e != nil {
+			// Broken JSON
+			if verbose {
+				fmt.Println("Job=%s", string(job.Data))
+			}
+
+			if skipOne {
+				fmt.Println("WARN: Skip job as JSON is invalid")
+				queue.Delete(job.Id)
+				skipOne = false
+				continue
+			}
+
+			// Ignore decode trouble
+			fmt.Println("CRIT: Invalid JSON received (msg=" + e.Error() + ")")
+			continue
+		}
+
+		if e := proc(m); e != nil {
+			// TODO: Isolate deverr from senderr
+			// Processing trouble?
+			fmt.Println("WARN: Failed sending, retry in 20sec (msg=" + e.Error() + ")")			
+			continue
+		}
+		queue.Delete(job.Id)
+	}
+	queue.Quit()
+}
