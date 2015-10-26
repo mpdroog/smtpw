df95a74b062f42ea05498238f7ec1015c3dcd869
mark <mpdroog@rootdev.nl>
2015-10-26 11:46:53 +0100

    Feature. rand string

---
+++ rand.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"time"
+	"math/rand"
+)
+
+var (
+	letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+)
+
+// Random string chars, n=length
+func RandText(n int) string {
+	rand.Seed(time.Now().Unix())
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
