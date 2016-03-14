cf475ce7776f285f26f4514515a904f508ea8720
MP Droog <mpdroog@icloud.com>
2016-03-14 10:36:18 +0100

    Test. Fill Beanstalkd queue for testing

---
+++ test/test.php
@@ -0,0 +1,18 @@
+<?php
+function msg($msg) {
+	echo $msg . "\n";
+}
+$json = file_get_contents("./test.json");
+$len = strlen($json);
+
+$fd = fsockopen("127.0.0.1", "11300", $errno, $errstr, 10);
+if (! $fd) {
+	die(sprintf("%d - %s", $errno, $errstr));
+}
+fwrite($fd, "use email\r\n");
+msg(stream_get_line($fd, 999999999999, "\r\n"));
+fwrite($fd, "put 100 0 1 $len\r\n");
+fwrite($fd, "$json\r\n");
+msg(stream_get_line($fd, 999999999999, "\r\n"));
+fwrite($fd, "quit\r\n");
+fclose($fd);
